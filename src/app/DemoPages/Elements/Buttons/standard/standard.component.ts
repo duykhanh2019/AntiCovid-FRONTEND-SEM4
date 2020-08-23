b8b85027d0109ca6befd9a872da2f0c928e4c67b
NguyenLanAnh2299 <43206459+NguyenLanAnh2299@users.noreply.github.com>
2020-08-23 22:09:58 +0700

    feature#016/ List user

--- src/app/DemoPages/Elements/Buttons/standard/standard.component.ts
+++ src/app/DemoPages/Elements/Buttons/standard/standard.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {User} from '../../../Model/user.model';
+import {StandardService} from './standard.service';
+
 
 @Component({
   selector: 'app-standard',
@@ -6,39 +9,19 @@ import {Component, OnInit} from '@angular/core';
   styles: []
 })
 export class StandardComponent implements OnInit {
+  datas: User[] = [];
+  constructor(private standardService: StandardService) { }
+  ngOnInit(): void {
+    this.getAll();
 
-  heading = 'Standard Buttons';
-  subheading = 'Wide selection of buttons that feature different styles for backgrounds, borders and hover options!';
-  icon = 'pe-7s-plane icon-gradient bg-tempting-azure';
-
-  progress: boolean | number = false;
-
-  model = {
-    left: true,
-    middle: false,
-    right: false
-  };
-
-  startLoading() {
-    this.progress = 0; // starts spinner
-
-    setTimeout(() => {
-      this.progress = 0.5; // sets progress bar to 50%
-
-      setTimeout(() => {
-        this.progress = 1; // sets progress bar to 100%
-
-        setTimeout(() => {
-          this.progress = false; // stops spinner
-        }, 200);
-      }, 500);
-    }, 400);
   }
-
-  constructor() {
+  getAll() {
+    this.standardService.getAll().subscribe(
+        (res: any) => {
+          this.datas = res;
+        },
+        error =>
+            console.log(3453)
+    );
   }
-
-  ngOnInit() {
-  }
-
 }
---
+++ src/app/DemoPages/Model/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+    id: number;
+    userName: string;
+    phone: number;
+    address: string;
+    email: string;
+}
