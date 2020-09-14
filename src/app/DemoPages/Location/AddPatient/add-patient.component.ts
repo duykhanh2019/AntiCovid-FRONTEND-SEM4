8804959ebfe027c90b97d3824587138474225a9b
duy khanh <giovotinh15758@gmail.com>
2020-09-14 22:36:52 +0700

    edit form add patient

--- src/app/DemoPages/Location/AddPatient/add-patient.component.ts
+++ src/app/DemoPages/Location/AddPatient/add-patient.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {PatientModel} from '../../Model/patient.model';
+import { AddPatientService } from './add-patient.service';
 
 @Component({
   selector: 'app-add-patient',
@@ -6,19 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-patient.component.sass']
 })
 export class AddPatientComponent implements OnInit {
+
   public isActive: any;
   private data: string[] = [];
+  addPatientForm: FormGroup;
+  submitted = false;
+  patientData: PatientModel[] = [];
 
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private addPatientService: AddPatientService) { }
 
   ngOnInit() {
-  }
+    this.getAll();
 
+    this.addPatientForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      verifyDate: ['', [Validators.required]]
+    });
+  }
+  getAll() {
+    this.addPatientService.getAll().subscribe(
+      (res: any) => {
+        this.patientData = res;
+      }
+    );
+  }
+  get f() { return this.addPatientForm.controls; }
   deleteMsg(msg: string) {
     const index: number = this.data.indexOf(msg);
     if (index !== -1) {
       this.data.splice(index, 1);
+    }
+  }
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.addPatientForm.invalid) {
+      return;
     }
   }
 
---
+++ src/app/DemoPages/Location/AddPatient/add-patient.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'Application/json'})
+};
+const apiUrl = 'http://52.165.229.168:8080/api/patients';
+const apiUrlDelete = 'http://52.165.229.168:8080/api/patient/';
+const apiUrlPatient = 'http://52.165.229.168:8080/api/patient/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddPatientService {
+  constructor(private httpClient: HttpClient) { }
+  getAll() {
+    return this.httpClient.get<any>(apiUrl).pipe();
+  }
+
+  public addPatient(objPatient) {
+
+    return this.httpClient.post<any>('http://52.165.229.168:8080/api/patients', objPatient);
+  }
+
+  Delete(id: number): Observable<any> {
+    return this.httpClient.delete(apiUrlDelete + id).pipe();
+  }
+  getPatient(id: number): Observable<any> {
+    return this.httpClient.get(apiUrlPatient + id).pipe();
+  }
+}
